d42d31732abc43ba366590aa9c66ce205bf3d5af
Derek Miranda <derek.miranda@ff0000.com>
2019-05-23 16:42:53 -0700

    package: initializing 320x320 BT

---
+++ README.md
@@ -0,0 +1,3 @@
+# Template: 300x250 - Centered Branding 
+
+This file should describe the creative-specs necessary to assemble this template. Those details should describe the Treatment properties, key-art sizes, layout guidelines, etc.
---
+++ src/3-traffic/default/320x320/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -0,0 +1,32 @@
+// GENERIC SOURCE TRACKER: bt-Endframe-Resolve
+if (typeof module === 'undefined') {
+	module = {}
+}
+// prettier-ignore
+module.exports = {
+  "id": "8c417243-849b-4868-8eb9-91063e2f3d80",
+  "name": "bt-ER-300x250",
+  "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
+  "type": "build_templates",
+  "builder": {
+    "label": "300x250 NF Global Template",
+    "tier": "endframe-resolve",
+    "size": "300x250",
+    "creative": "./dist/3-traffic/default/300x250",
+    "toolkit": "./toolkit/300x250_NF_Global_Template_ContainerRecycle_Layered.psd",
+    "layouts": {
+      "SIDE_BY_SIDE": "Side By Side",
+      "CORNER_LEFT": "Corner Left",
+      "CORNER_RIGHT": "Corner Right",
+      "ONE_LINE": "One Line"
+    }
+  },
+  "context": "RED Interactive",
+  "state": "published",
+  "updated": 1557969942685,
+  "username": "GitHub",
+  "full_name": "NetflixDev/bt-ER-300x250",
+  "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
+  "version": "2.2.1",
+  "minimum": "2.2.1"
+};
