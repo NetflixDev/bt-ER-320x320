35c42f2c91c94767ff6cfd4ced47198e07807c6e
Derek Miranda <derek.miranda@ff0000.com>
2019-09-09 21:38:24 -0700

    Mobile intro zoom fix

--- dist/1-build/common/js/EndFrame/inits/index.js
+++ dist/1-build/common/js/EndFrame/inits/index.js
@@ -1,34 +1,26 @@
-import { Styles, Markup, Align, Effects } from "ad-view";
-import { ImageManager } from "ad-control";
-import { Animation } from "@common/js/Animation.js";
-import { Control } from "@common/js/Control.js";
-import CanvasIris from "@common/js/CanvasIris.js";
-import {
-  UIComponent,
-  UIBorder,
-  UIButton,
-  UIImage,
-  TextFormat,
-  UITextField,
-  UISvg
-} from "ad-ui";
-import { ObjectUtils } from "ad-utils";
-import baseInit from "./baseInit.js";
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { ImageManager } from 'ad-control'
+import { Animation } from '@common/js/Animation.js'
+import { Control } from '@common/js/Control.js'
+import CanvasIris from '@common/js/CanvasIris.js'
+import { UIComponent, UIBorder, UIButton, UIImage, TextFormat, UITextField, UISvg } from 'ad-ui'
+import { ObjectUtils } from 'ad-utils'
+import baseInit from './baseInit.js'
 
-export { sideBySideInit, cornerInit, oneLineInit };
+export { sideBySideInit, cornerInit, oneLineInit }
 
 function sideBySideInit(T) {
-  baseInit(T, { logoWidth: 90, ctaWidth: 90, ctaHeight: 24 });
+	baseInit(T, { logoWidth: 90, ctaWidth: 90, ctaHeight: 24 })
 }
 
 function cornerInit(T) {
-  const extraCtaLogoInit = (cta, logo) => {
-    cta.setAttribute("min-font-size", 6);
-    cta.setAttribute("stretch-origin", "right");
-  };
-  baseInit(T, { logoWidth: 75, ctaWidth: 75, ctaHeight: 20, extraCtaLogoInit });
+	const extraCtaLogoInit = (cta, logo) => {
+		cta.setAttribute('min-font-size', 6)
+		cta.setAttribute('stretch-origin', 'right')
+	}
+	baseInit(T, { logoWidth: 75, ctaWidth: 75, ctaHeight: 20, extraCtaLogoInit })
 }
 
 function oneLineInit(T) {
-  baseInit(T, { logoWidth: 75, ctaWidth: 83, ctaMaxWidth: 90, ctaHeight: 22 });
+	baseInit(T, { logoWidth: 75, ctaWidth: 83, ctaMaxWidth: 90, ctaHeight: 22 })
 }
